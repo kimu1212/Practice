ed144ee22492ae6209db670d30a6f4103fd87ab3
木村拓也 <kimuratakuya@Mac.local>
2018-03-09 19:16:39 +0900

    add Entity

---
+++ src/main/java/com/practice/song/entity/SongConfig.java
@@ -0,0 +1,26 @@
+package com.practice.song.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class SongConfig {
+
+    @Id
+    private int id;
+    private String name;
+    private int status;
+
+    public String getName(){
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public int getStatus(){
+        return status;
+    }
+    
+}
